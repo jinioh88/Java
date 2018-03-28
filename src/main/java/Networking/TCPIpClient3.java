20101aedfa62f3ab51a9c6186fb6484deafd04d5
OHSEJIN <jinio88@gmail.com>
2018-03-28 23:46:35 +0900

    채팅

--- src/main/java/Networking/TCPIpClient3.java
+++ src/main/java/Networking/TCPIpClient3.java
@@ -6,24 +6,18 @@ import java.io.InputStream;
 import java.net.ConnectException;
 import java.net.Socket;
 
-public class TCPIpClient {
+public class TCPIpClient3 {
     public static void main(String[] args) {
         try{
             String serverIp = "127.0.0.1";
-            System.out.println("서버에 연결중...");
+            Socket socket = new Socket(serverIp,7777);
+            System.out.println("연결성공!");
 
-            Socket socket = new Socket(serverIp,7778);
+            Sender sender = new Sender(socket);
+            Receiver receiver = new Receiver(socket);
 
-
-            // 입력 스트림 얻기
-            InputStream in = socket.getInputStream();
-            DataInputStream ds = new DataInputStream(in);
-
-            System.out.println("서버로부터 받은 메시지 : "+ds.readUTF());
-            System.out.println("Bye~!");
-
-            ds.close();
-            socket.close();
+            sender.start();
+            receiver.start();
         }catch(ConnectException ce){ce.printStackTrace();
         }catch(IOException ie){
             ie.printStackTrace();
