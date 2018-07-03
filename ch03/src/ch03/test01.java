5b7eb6670fe3d57b6a2abb84cf3e2e0800167fa3
오세진 <jinioh88@gmail.com>
2018-07-03 23:34:23 +0900

    팩토리 메서드 /

--- ch03/src/ch03/test01.java
+++ ch03/src/ch03/test01.java
@@ -1,5 +1,25 @@
 package ch03;
 
+import java.text.NumberFormat;
+
 public class test01 {
+	
+	// 정적 메서
+//	public static double pow(double base, double expon) {
+//	
+//		double d=0.0;
+//		
+//		return d;
+//	}
+	
+	public static void main(String[] args) {
+		// 팩토리 메서드 
+		NumberFormat currentFormmat = NumberFormat.getCurrencyInstance();
+		NumberFormat percentFormmat = NumberFormat.getPercentInstance();
+		double d = 0.1;
+		System.out.println(currentFormmat.format(d));
+		System.out.println(percentFormmat.format(d));
+	}
+	
 	
 }
