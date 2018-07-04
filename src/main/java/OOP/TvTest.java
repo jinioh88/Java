14db5fe698b5dea8a025a997d6c03caefae4f0a5
오세진 <jinioh88@gmail.com>
2018-07-04 11:59:48 +0900

    클래스 변수

--- src/main/java/OOP/TvTest.java
+++ src/main/java/OOP/TvTest.java
@@ -17,6 +17,12 @@ public class TvTest {
         Tv t1 = new Tv();  // t1 참조 변수에 Tv인스턴스만들고 주소 대입.
         Tv t2 = new Tv();
         t1.channel = 7;
+
+        Tv[] tvArr = new Tv[2];  // 객체 배열.
+        tvArr[0] = new Tv();
+        tvArr[1] = new Tv();
+
+        Tv[] tvArr2 = {new Tv(),new Tv()};
     }
 }
 
