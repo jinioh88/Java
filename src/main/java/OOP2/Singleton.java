720e45bdaf0ce4bf4e17aad8313e4d4b0381dd46
오세진 <jinioh88@gmail.com>
2018-07-05 15:20:43 +0900

    생성자 접근제어자

---
+++ src/main/java/OOP2/Singleton.java
@@ -0,0 +1,17 @@
+package OOP2;
+
+
+final class Singleton {
+    private static Singleton s = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(s==null)
+            s = new Singleton();
+
+        return s;
+    }
+}
