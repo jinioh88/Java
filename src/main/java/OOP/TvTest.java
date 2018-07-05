4f04efc7d25544b6c30b030816901a57f0f9b32a
오세진 <jinioh88@gmail.com>
2018-07-05 13:00:46 +0900

    상속

--- src/main/java/OOP/TvTest.java
+++ src/main/java/OOP/TvTest.java
@@ -14,15 +14,15 @@ class Tv{
 
 public class TvTest {
     public static void main(String[] args) {
-        Tv t1 = new Tv();  // t1 참조 변수에 Tv인스턴스만들고 주소 대입.
-        Tv t2 = new Tv();
+        OOP2.Tv t1 = new OOP2.Tv();  // t1 참조 변수에 Tv인스턴스만들고 주소 대입.
+        OOP2.Tv t2 = new OOP2.Tv();
         t1.channel = 7;
 
-        Tv[] tvArr = new Tv[2];  // 객체 배열.
-        tvArr[0] = new Tv();
-        tvArr[1] = new Tv();
+        OOP2.Tv[] tvArr = new OOP2.Tv[2];  // 객체 배열.
+        tvArr[0] = new OOP2.Tv();
+        tvArr[1] = new OOP2.Tv();
 
-        Tv[] tvArr2 = {new Tv(),new Tv()};
+        OOP2.Tv[] tvArr2 = {new OOP2.Tv(),new OOP2.Tv()};
     }
 }
 
---
+++ src/main/java/OOP2/Tv.java
@@ -0,0 +1,27 @@
+package OOP2;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power(){ power = !power; }
+    void channelUp(){ ++channel; }
+    void channelDown(){ --channel; }
+}
+
+class CaptionTv extends Tv {
+    boolean caption;
+    void displayCaption(String text){
+        if(caption)
+            System.out.println(text);
+    }
+}
+
+class CaptionTvTest {
+    public static void main(String args[]) {
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+    }
+}
