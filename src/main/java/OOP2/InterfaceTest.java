717ebd882059add3b5cc9a4c4bb8a691cf0afec9
오세진 <jinioh88@gmail.com>
2018-07-06 13:22:11 +0900

    Interface Test

---
+++ src/main/java/OOP2/InterfaceTest.java
@@ -0,0 +1,34 @@
+package OOP2;
+
+class InterfaceTest {
+    public static void main(String[] args) {
+        A a = new A();
+        a.methodA();
+    }
+}
+
+class A {
+    void methodA() {
+        I i = InstanceManager.getInstance();
+        i.methodB();
+        System.out.println(i.toString());
+    }
+}
+
+interface I {
+    public abstract void methodB();
+}
+
+class B implements I {
+    public void methodB(){
+        System.out.println("methodB...");
+    }
+
+    public String toSTring() {return "ClassB...";}
+}
+
+class InstanceManager {
+    public static I getInstance() {
+        return new B();
+    }
+}
