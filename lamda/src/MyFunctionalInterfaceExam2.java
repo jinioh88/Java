538724b25be2c15c7830742636ba2b319b255dfb
오세진 <jinioh88@gmail.com>
2018-08-03 13:44:12 +0900

    람다 멤버변수

--- lamda/src/MyFunctionalInterfaceExam2.java
+++ lamda/src/MyFunctionalInterfaceExam2.java
@@ -1,6 +1,24 @@
 public class MyFunctionalInterfaceExam2 {
+    public int outterField = 10;
+    // 클래스 멤버 사용
+    class Inner {
+        int innerField = 20;
+
+        void method() {
+            MyFunctionlInterface f1 = () -> {
+                System.out.println("outField : "+ outterField);
+                System.out.println("outter this : "+ MyFunctionalInterfaceExam2.this.outterField);
+
+                System.out.println("innerField : "+innerField);
+                System.out.println("inner : "+this.innerField);
+            };
+            f1.method();
+        }
+    }
+
     public static void main(String[] args) {
         MyFunctionlInterface2 fi;
+
 
         fi = (x) -> {
           int result = x * 5;
@@ -14,5 +32,23 @@ public class MyFunctionalInterfaceExam2 {
 
         fi = x -> System.out.println(x*7);
         fi.method(2);
+
+        // 리턴값 있는 람다식
+        MyFunctionlInterface3 f3 = (x,y) -> {
+          int result = x+y;
+          return result;
+        };
+        System.out.println(f3.method(1,2));
+
+        f3 = (x,y) -> {return x+y;};
+        System.out.println(f3.method(1,2));
+
+        f3 = (x,y) -> sum(x,y);
+
+
+    }
+
+    public static int sum(int a, int b) {
+        return a+b;
     }
 }
---
+++ lamda/src/MyFunctionlInterface3.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface MyFunctionlInterface3 {
+    public int method(int x, int y);
+}
