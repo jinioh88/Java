f5881699aa649c9dcf1efec4c44024a8f2fbb4ca
ohsejin <jinioh88@gmail.com>
2018-09-25 22:53:52 +0900

    옵저버패턴

---
+++ DesignPattern/src/observer/WeatherStation.java
@@ -0,0 +1,12 @@
+package observer;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(80,65,30.4f);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        weatherData.setMeasurements(80,65,30.4f);
+    }
+}
